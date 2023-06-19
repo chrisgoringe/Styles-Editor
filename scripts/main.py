7475ca98f6c966e38ad0a8477b5c7f0f885e25d5
Chris <chris.goringe@gmail.com>
2023-06-19 19:40:46 +1000

    Revert "new row is copy of selected row #10"
    This reverts commit 51a40723db110cf34ee588eada15fc968dc8274f.

--- scripts/main.py
+++ scripts/main.py
@@ -22,13 +22,9 @@ class Script(scripts.Script):
     return ()
   
 class StyleEditor:
-  update_help = """ # Plugin updated
-  
-## Changed in this update:
-
-- Right-click can be used to select a row in the table (a style).
-- Delete the selected style by pressing `backspace`/`delete`.
-- Click `new row` with a row selected to duplicate prompt and negative prompt.
+  update_help = """ # Changed in this update:
+- Right-click can be used to select a row in the table (a style)
+- Delete the selected style by pressing `backspace`/`delete`
 """
   cols = ['name','prompt','negative_prompt']
   full_cols = ['sort', 'name','prompt','negative_prompt']
