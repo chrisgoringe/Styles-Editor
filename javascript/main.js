6dde2a454017b1f90d71ce45b91dc08085aec06e
Chris <chris.goringe@gmail.com>
2023-07-03 12:49:52 +1000

    JSON parse response

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,7 @@
 function api_post(path, payload, callback) {
     var xhr = new XMLHttpRequest();
     xhr.open("POST", path, false);
-    xhr.onreadystatechange = function() { if (xhr.readyState === 4 && xhr.status === 200) { callback(xhr.responseText); } }
+    xhr.onreadystatechange = function() { if (xhr.readyState === 4 && xhr.status === 200) { callback(JSON.parse(xhr.responseText)); } }
     xhr.setRequestHeader("Content-type", "application/json");
     xhr.send(JSON.stringify(payload));
 }
